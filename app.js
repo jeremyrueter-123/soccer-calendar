146a5b7f35268eb804e7650a20515046083f4374
jeremyrueter-123 <jeremyrueter@gmail.com>
2026-09-15 22:22:14 -0400

    back to where we started

--- app.js
+++ app.js
@@ -432,14 +432,6 @@ function renderMatches(matches) {
 
 
     const grouped = {};
-
-    const today = new Date();
-today.setHours(0, 0, 0, 0);
-
-const todayString =
-    today.getFullYear() + "-" +
-    String(today.getMonth() + 1).padStart(2, "0") + "-" +
-    String(today.getDate()).padStart(2, "0");
     
     matches.forEach(match => {
 
