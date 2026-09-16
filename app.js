0b6806e70bd3ef04af7ceed3b8123bca283aca31
jeremyrueter-123 <jeremyrueter@gmail.com>
2026-09-15 22:09:20 -0400

    fixed some script

--- app.js
+++ app.js
@@ -440,8 +440,6 @@ const todayString =
     today.getFullYear() + "-" +
     String(today.getMonth() + 1).padStart(2, "0") + "-" +
     String(today.getDate()).padStart(2, "0");
-
-    const today = new Date().toISOString().split("T")[0];
     
     matches.forEach(match => {
 
